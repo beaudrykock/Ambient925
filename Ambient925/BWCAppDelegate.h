59faca1456dd50c5bcf6bf7aaeb4e5e849576b08
Beaudry Kock <beaudry.kock@mac.com>
2012-10-28 17:14:45 +0000

    First commit

---
+++ Ambient925/BWCAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  BWCAppDelegate.h
+//  Ambient925
+//
+//  Created by Beaudry Kock on 10/28/12.
+//  Copyright (c) 2012 Better World Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
